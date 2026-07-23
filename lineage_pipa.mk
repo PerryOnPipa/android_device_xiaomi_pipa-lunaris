bd844017b5883a750c27475d73dd317b1efb716a
Null Pointer <nullpointer1101@outlook.com>
2026-07-23 03:43:27 +0000

    pipa: Bringup AxionAosp!

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -14,8 +14,16 @@ $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
-# Inherit keys
-$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+# Axion Flags
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_AXFX := false
+AXION_CAMERA_REAR_INFO := 13
+AXION_CAMERA_FRONT_INFO := 8
+AXION_PROCESSOR := Snapdragon_870
+AXION_MAINTAINER := nullpointer1101
+TARGET_INCLUDES_LOS_PREBUILTS := false
+TARGET_INCLUDE_PARTNER_SETUP := true
+TARGET_INCLUDE_GOOGLE_TELECOMM := false
 
 PRODUCT_NAME := lineage_pipa
 PRODUCT_DEVICE := pipa
